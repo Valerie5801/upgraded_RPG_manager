4861cf286258f38f3574b5691b8180c6404113f1
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-01-21 10:50:17 -0700

    level info dictionary

---
+++ level_info.py
@@ -0,0 +1,210 @@
+levels = {
+    "cleric": {
+        "1": {
+            'skills': ['healing','buff magic'],
+            'skill points': 3,
+            'abilities': 0
+        },
+        "2": {
+            'skills': [],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "3": {
+            'skills': ['smiting'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "4": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "5": {
+            'skills': [],
+            'skill points': 0,
+            'abilities': 2
+        },
+        "6": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "7": {
+            'skills': ['defense boost'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "8": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "9": {
+            'skills': ['holy aura'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "10": {
+            'skills': [],
+            'skill points': 3,
+            'abilities': 0
+        }
+    },
+    "rogue": {
+        "1": {
+            'skills': ['stealth','thievery'],
+            'skill points': 3,
+            'abilities': 0
+        },
+        "2": {
+            'skills': [],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "3": {
+            'skills': ['sneak attack'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "4": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "5": {
+            'skills': [],
+            'skill points': 0,
+            'abilities': 2
+        },
+        "6": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "7": {
+            'skills': ['speed boost'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "8": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "9": {
+            'skills': ['assassinate'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "10": {
+            'skills': [],
+            'skill points': 3,
+            'abilities': 0
+        }
+    },
+    "wizard": {
+        "1": {
+            'skills': ['attack magic','buff magic'],
+            'skill points': 3,
+            'abilities': 0
+        },
+        "2": {
+            'skills': [],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "3": {
+            'skills': ['control magic'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "4": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "5": {
+            'skills': [],
+            'skill points': 0,
+            'abilities': 2
+        },
+        "6": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "7": {
+            'skills': ['aoe magic'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "8": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "9": {
+            'skills': ['ultimate magic'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "10": {
+            'skills': [],
+            'skill points': 3,
+            'abilities': 0
+        }
+    },
+    "fighter": {
+        "1": {
+            'skills': ['physical attack','speed boost'],
+            'skill points': 3,
+            'abilities': 0
+        },
+        "2": {
+            'skills': [],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "3": {
+            'skills': ['extra attack'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "4": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "5": {
+            'skills': [],
+            'skill points': 0,
+            'abilities': 2
+        },
+        "6": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "7": {
+            'skills': ['defense boost'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "8": {
+            'skills': [],
+            'skill points': 2,
+            'abilities': 0
+        },
+        "9": {
+            'skills': ['action surge'],
+            'skill points': 1,
+            'abilities': 0
+        },
+        "10": {
+            'skills': [],
+            'skill points': 3,
+            'abilities': 0
+        }
+    }
+}
