4a52374a26c518fbfc8dda17a97cfc006ac16a1d
Valerie5801 <valerie.yang@ucas-edu.net>
2026-01-26 17:40:32 +0000

    Did as much as I could for the menu and inputs (used pseudocode as direct reference)

---
+++ menu.py
@@ -0,0 +1,23 @@
+#VY 2nd Menu
+from helper import u_input
+
+def main_menu():
+    while True:
+        print("You can: \n1. Create a Character \n2. Edit a Character \n3. View a Character \n4. Exit the Program(This will wipe all data!)")
+        user_choice = input("Type the number that corresponds with the action that you want to perform: ").strip()
+        
+        match user_choice:
+            case "1":
+                #run character creation
+            case "2":
+                #when all other functions exist, put them here.
+            case "3":
+                #show character function here
+            case "4":
+                break
+            case _:
+                print("That isn't an option, please try again.")
+
+print("This is a RPG Character Manager.")
+main_menu()
+print("Thank you for using the Character Manager.")
