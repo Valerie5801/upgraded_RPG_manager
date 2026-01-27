8c26987b9f42562e92375eabb0355223624ef577
Valerie5801 <valerie.yang@ucas-edu.net>
2026-01-27 17:10:57 +0000

    Fixed menu a bit and finished the user inputs section

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ from helper import u_input
 def main_menu():
     while True:
         print("You can: \n1. Create a Character \n2. Edit a Character \n3. View a Character \n4. Exit the Program(This will wipe all data!)")
-        user_choice = input("Type the number that corresponds with the action that you want to perform: ").strip()
+        user_choice = u_input("Type the number that corresponds with the action that you want to perform: ")
         
         match user_choice:
             case "1":
