f59aa6f48b27122955bbce32da1ec0ae9b539ffd
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-01-30 17:36:54 +0000

    finished dice

--- dice.py
+++ dice.py
@@ -6,11 +6,9 @@ import random
 
 def die(sides):
     def amountRolled(ammount):
-        dice = {
-
-        }
+        dice = []
         for i in range(ammount):
-            dice[i] = random.randint(1, sides)
+            dice.append(random.randint(1, sides))
 
         return dice
     
@@ -24,6 +22,40 @@ def rolling():
     percentile = die(100)
     d12 = die(12)
     d20 = die(20)
-    choice_input(['1','2','3','4','5','6','7'])
 
+
+    def rolled(die):
+
+        def showing(already_rolled):
+            for i in already_rolled:
+                print(f"one die rolled a {i}")
+
+            print(f"all dice added together is {sum(already_rolled)}")
+
+
+
+        sides = int_input(1000000000000, "how many dice are you rolling?\n Input here:")
+        if die == '1':
+            showing(d4(sides))
+        elif die == '2':
+            showing(d6(sides))
+        elif die == '3':
+            showing(d8(sides))
+        elif die == '4':
+            showing(d10(sides))
+        elif die == '5':
+            showing(percentile(sides))
+        elif die == '6':
+            showing(d12(sides))
+        elif die == '7':
+            showing(d20(sides))
+
+
+
+    die_pick = choice_input(['1','2','3','4','5','6','7'], f"Press 1 if you would like to roll a D4 (die with four sides)\nPress 2 if you would like to use a D6 (die with 6 sides)\nPress 3 if you would like to use a D8 (die with 8 sides)\nPress 4 to use a D10 (die with 10 sides)\nPress 5 to use the Percentile die (die with 100 sides)\nPress 6 for the D12 (die with 12 sides)\nPress 7 for a D20 (die with 20 sides)\n Input here: ")
+
+    rolled(die_pick)
+
+rolling()
+    
     
